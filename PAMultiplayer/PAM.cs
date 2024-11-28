595ef9acdedcf9eb8a63339ba46d64148556588f
Aiden-ytarame <116085315+Aiden-ytarame@users.noreply.github.com>
2024-11-27 21:47:38 -0300

    version number

--- PAMultiplayer/PAM.cs
+++ PAMultiplayer/PAM.cs
@@ -23,7 +23,7 @@ public class PAM : BasePlugin
     Harmony harmony;
     const string Guid = "me.ytarame.Multiplayer";
     const string Name = "Multiplayer";
-    public const string Version = "0.8.2.1";
+    public const string Version = "0.8.2.2";
 
     public override void Load()
     {
